1fd4feefbd655c45390eb2b6028bd1589b17baa9
Stefano Cosentino <stefano_cosentino@apple.com>
2023-11-10 07:26:29 -0800

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # tennis_rank
+> Stefano Nov 2023
+
+The script `compute_tennis_score.py` performs the following tasks:
+- Crawl tennisrungs for the specified league (no API key necessary)
+- Parse all results
+- Compute the [ELO score](https://en.wikipedia.org/wiki/Elo_rating_system)
+- Estimate a [USTA score](https://www.usta.com/en/home/play/adult-tennis/programs/national/usta-ntrp-ratings-faqs.html) (also known as NTRP)
+
+
+For the latter estimation, the following steps/assumptions are followed:
+
+- Assume each group is distributed normally and centered to the USTA range; eg, the average player in the 3.0-3.5 group has a USTA score of 3.25.
+- Assume that at the end of the tournament, top 5% will move up, bottom 5% will move down (actual numbers may vary, this is just for a completely filled in distribution)
+- The rest is just math to shift from ELO to USTA score distribution
+- Finally, apply a compression to ensure that all values are at best +/- 0.5 USTA levels from the range limits; eg for 3.0-3.5 group, no score smaller than 2.5 or greater than 4.0 is generated. This compression is a power of 10. 
+
+## Run
+
+```
+python compute_tennis_score.py --league_id singles-3.0-3.5/143889747 --save --verbose
+```
+
+## Install
+
+```
+pip install -r requirements.txt
+```
+
+
+## Output
+![alt text](img/Screenshot2023-11-09.png "Terminal output")
--- compute_tennis_score.py
+++ compute_tennis_score.py
@@ -1,42 +1,58 @@
+import re
+import sys
 import requests
+import numpy as np
 import pandas as pd
+from loguru import logger
+from scipy.stats import norm
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
-import sys
-from loguru import logger
 
 
 URL_BASE = "https://app.tennisrungs.com"
-    
+
 
 class ComputeElo:
 	'''
-	Class to implement the Elo rating. Formulas & parameters are taken from:
-	https://en.wikipedia.org/wiki/Elo_rating_system
+	Class to implement the Elo score_elo. Formulas & parameters are taken from:
+	https://en.wikipedia.org/wiki/Elo_score_elo_system
 	'''
 	def __init__(self, k=20, g=5):
 		self.elo = {}
+		self.usta_scores = {}
 		self.elo_history = []
+		self.players = {}
 		self.k = k  # this should be >20 when we have small number of matches
 		self.g = g  # dunno about this
 
-	def add_players(self, names, rating=1500):
-		for name in names: self.elo[name] = rating
+	def add_players(self, names, score_elo=1500):
+		for name in names:
+			self.players[name] = Player(name=name, score_elo=score_elo)
+			self.elo[name] = score_elo
 
 	def expected_outcome(self, p1, p2, a=400.0, b=10.0):
 		return 1 / (b**((p2 - p1) / a) + 1)
 
-	def log_result(self, winner, loser):
+	def log_result(self, winner, loser, min_=100):
 		exp_result = self.expected_outcome(self.elo[winner], self.elo[loser])
-		self.elo[winner] = max(100, self.elo[winner] + (self.k * self.g) * (1 - exp_result))
-		self.elo[loser]  = max(100, self.elo[loser]  + (self.k * self.g) * (exp_result - 1))
+		kg = self.k * self.g
+		self.elo[winner] = max(min_, self.elo[winner] + kg * (1 - exp_result))
+		self.elo[loser]  = max(min_, self.elo[loser]  + kg * (exp_result - 1))
 		self.elo_history.append(list(self.elo.values()))
 
-	def rankings(self):
-		return sorted(self.elo.items(), key=lambda item: -item[1])
+		self.update_player_stats(winner, result='W')
+		self.update_player_stats(loser,  result='L')
 
-	def top(self, N=None, verbose=False):
-		rankings = self.rankings()[:N or len(self.elo)]
+	def update_player_stats(self, name, result):
+		self.players[name].score_elo = self.elo[name]
+		self.players[name].results += result
+
+	def rankings(self, score='elo'):
+		scores = self.usta_scores if score=='usta' else self.elo
+		return sorted(scores.items(), key=lambda item: -item[1])
+
+	def top(self, N=None, verbose=False, score='elo'):
+		rankings = self.rankings(score=score)[:N or -1]
 		if verbose:
 			for n, (a, b) in enumerate(rankings, 1):
 				print(f"#{n} {b:.0f} {a}")
@@ -46,6 +62,53 @@ class ComputeElo:
 		df = pd.DataFrame(self.elo_history)
 		df.columns = self.elo.keys()
 		return df
+	
+	def compute_usta_scores(self, low_cutoff, hig_cutoff, pc=0.95, map_to=0.5, compress=True):
+
+		score_usta = np.array(np.array(list(self.elo.values())), copy=True)
+		score_usta /= score_usta.std()
+		score_usta -= score_usta.mean()
+		score_usta += low_cutoff + (hig_cutoff - low_cutoff) / 2.0
+
+		cutoff_up = norm.ppf(pc, loc=score_usta.mean(), scale=score_usta.std())
+		scale = cutoff_up - score_usta.mean()
+
+		mu = score_usta.mean()
+		score_usta -= mu
+		score_usta /= scale
+		score_usta += mu
+
+		if compress:
+			x_ = score_usta[score_usta>hig_cutoff]
+			min_ = x_.min()
+			x_ = np.log10(x_ / min_)
+			x_ = (x_ / x_.max() * map_to) + min_
+			score_usta[score_usta>hig_cutoff] = x_
+
+			x_ = score_usta[score_usta<low_cutoff]
+			max_ = x_.max()
+			x_ = 10**(x_ / max_ - 1)  #np.log10(vals / v_m)
+			x_ -= x_.min()
+			x_ = (low_cutoff-map_to) + x_ / x_.max() * map_to
+			score_usta[score_usta<low_cutoff] = x_
+		
+		for k, name in enumerate(self.elo.keys()):
+			self.players[name].score_usta = score_usta[k]
+			self.usta_scores[name] = score_usta[k]
+
+
+class Player:
+	'''Class for the Player.'''
+	def __init__(self, name, score_elo):
+		self.name = name
+		self.score_elo = score_elo
+		self.score_usta = None
+		self.results = ''
+		self.points = None
+
+	@property
+	def last_result(self):
+		return self.results[-1]
 
 def parse_table(table):
     '''Parse table from scraped html.'''
@@ -59,28 +122,33 @@ def parse_table(table):
         data.append(row_data)
     return data[1:]
 
-def calc_ratio_win(s, add_tb=False):
-    '''Needs to be improved. Extract winning stats.'''
-    if 'Forfeit' in s:
-        # Ignore forfeit
-        return 0.
-    groups = [g.strip() for g in s.split(',')]
+def calculate_result_stats(s, add_tb=False, option=1):
+    '''Extract winning stats.'''
+    for lbl in ['Forfeit', 'NoShow', 'CourtTimeExpired']:
+        s = s.replace(lbl, '')
+    rex = r'([0-9]{1,2}\-[0-9]{1,2})?\s?(\(([0-9]{1,2}\-[0-9]{1,2})\))?'
     num, den = 0, 0
-    for g in groups[:2]:
-        a, b = g[:3].split('-')
-        num += int(a)
-        den += int(a) + int(b)
-    # if add_tb and len(groups)==3:
-    #     if groups[2][0]=='(':
-    #         g, d = groups[2][1:-1], 4
-    #     else:
-    #         g, d = groups[2], 1
-    #     a, b = g.split('-')
-    #     # Roughly equate 1 TB won to 2.5 games
-    #     num += 2.5
-    #     den += 2.5
+    for g in s.split(','):
+        res, _, tb = re.match(rex, g.strip()).groups()
+        if res is not None:
+            a, b = res.split('-')
+            num += int(a)
+            den += int(a) + int(b)
+            # Ignore TB score, since the game has already been recorded
+        else:
+            # Super TB (no res)
+            if not add_tb:
+                continue
+            if option == 1:
+                # Option 1: Roughly equate 1 TB won to 2.5 games
+                num += 2.5
+                den += 2.5
+            if option == 2:
+                # Option 2: Roughly equate 5 points to 1 game won
+                a, b = res.split('-')
+                num += int(a) // 5
+                den += int(a) // 5 + int(b) // 5
     return num/den if den>0 else 0
-
 
 def cleanup_result_logs(df, df_player):
     df.columns = ['player_a_id', 'player_a',  'date', 'player_b', 'link', 'res', 'score']
@@ -96,11 +164,11 @@ def cleanup_result_logs(df, df_player):
     df['loser'] = df['res'].apply(lambda x: 'b' if x=='W' else 'a')
     df['winner_name'] = df.apply(lambda x: x[f'player_{x["winner"]}'], axis=1)
     # Create winning stats, maybe useful in the future:
-    df['ratio_win'] = df['score'].apply(lambda x: calc_ratio_win(x))
+    df['ratio_win'] = df['score'].apply(lambda x: calculate_result_stats(x))
     df['ratio_win'] = df.apply(lambda x: x['ratio_win'] if x['res']=='W' else 1-x['ratio_win'], axis=1)
-    df['ratio_win_tb'] = df['score'].apply(lambda x: calc_ratio_win(x, add_tb=True))
+    df['ratio_win_tb'] = df['score'].apply(lambda x: calculate_result_stats(x, add_tb=True))
     df['ratio_win_tb'] = df.apply(lambda x: x['ratio_win_tb'] if x['res']=='W' else 1-x['ratio_win_tb'], axis=1)
-    # Unique identidifer for a match:
+    # Unique identifier for a match:
     df['match_uid'] = df.apply(lambda d: f"{d['date'].replace('/', '')}_{'_'.join(sorted(d[['player_a_id', 'player_b_id']]))}", axis=1)
     # Sort chronologically:
     df['date'] = pd.to_datetime(df['date'])
@@ -138,8 +206,7 @@ def crawl_results_pages(df_player):
             res_ = parse_table(table)
             res_ = [[row['player_id'], row['name']] + r for r in res_]
             results.append(res_)
-    df = pd.DataFrame([y for x in results for y in x])
-    return df
+    return pd.DataFrame([y for x in results for y in x])
 
 
 def main(args):
@@ -147,35 +214,55 @@ def main(args):
     logger.info(f"Crawling data for {args.league_id} and computing ELO score for all players.")
     
     # Crawl players
-    logger.info("[1/3] Crawling list of players ...")
+    logger.info("[1/4] Crawling list of players ...")
     df_player = crawl_players_page(args)
 
     # Create history by crawling all pages and logging results
-    logger.info("[2/3] Creating log with all matches played ...")
+    logger.info("[2/4] Creating log with all matches played ...")
     df_results = crawl_results_pages(df_player)
     df_results = cleanup_result_logs(df_results, df_player)
     logger.info(f'Found {len(df_player)} players with {len(df_results)} matched played.')
 
     # Compute ELO score
-    logger.info("[3/3] Calculate ELO score for all players ...\n")
+    logger.info("[3/4] Calculate ELO score for all players ...")
     CE = ComputeElo()
-    CE.add_players(df_player['name'], rating=1500)
+    CE.add_players(df_player['name'], score_elo=1500)
     # Iterate through all records and update elo's
     for idx, row in df_results.sort_values('ts').iterrows():
         CE.log_result(winner=row[f'player_{row["winner"]}'],
                       loser=row[f'player_{row["loser"]}'])
+    
+    # Compute USTA scores from elo's
+    logger.info("[4/4] Estimate USTA score for all players ...")
+    rex = r'([1-5]\.[0,5])\-([1-5]\.[0,5])'
+    low_cutoff, hig_cutoff = map(float, re.findall(rex, args.league_id)[0])
+    CE.compute_usta_scores(low_cutoff, hig_cutoff, pc=0.95, map_to=0.5, compress=True)
 
-    # Print ranks
-    rankings = CE.top(verbose=False)
-    logger.info(f'Listing all {len(df_player)} players and their ELO score:')
-    for n, (a, b) in enumerate(rankings, 1):
-        logger.info(f"#{n} {b:.0f} {a}")
+    # Save results
+    if args.save:
+        logger.info("Saving results to data/")
+        safe_filename = "".join([x if x.isalnum() else "_" for x in args.league_id])
+        logger.debug("Saving match results dataframe")
+        df_results.to_csv(f'data/results_{safe_filename}.csv')
+        logger.debug("Saving players dataframe")
+        df_player.to_csv(f'data/players_{safe_filename}.csv')
+        logger.debug("Saving score class")
+        np.save(f'data/scores_{safe_filename}.npy', CE)
+
+    # Print ranks and scores
+    rankings = CE.top(verbose=False, score='elo')
+    logger.info(f'Listing all {len(df_player)} players and their ELO/USTA scores:')
+    logger.info(" #\tELO\tUSTA\tname")
+    for n, (name, elo) in enumerate(rankings, 1):
+        logger.info(f"({n})\t{elo:.0f}\t{CE.usta_scores[name]:.2f}\t{name}")
+    logger.debug("All Done")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--league_id", default="singles-3.0-3.5/143889747",
                         help="Required positional argument")
+    parser.add_argument("-s", "--save", action="store_true")
     parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
 
